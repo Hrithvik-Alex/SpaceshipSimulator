225ef3bcb05965ef40c0a0d58bb210a553e50cb3
Rafit <rf2jamil@uwaterloo.ca>
2018-09-27 14:14:01 -0400

    Added propulsion subsystem controller code

--- Assets/Student Scripts/PropulsionSubsystemController.cs
+++ Assets/Student Scripts/PropulsionSubsystemController.cs
@@ -8,6 +8,31 @@ public class PropulsionSubsystemController
 {
     public void PropulsionUpdate(SubsystemReferences subsystemReferences, ThrusterControls thrusterControls)
     {
-
+        string currentSystem = subsystemReferences.currentGalaxyMapNodeName;
+        UnityEngine.Debug.Log(currentSystem);
+        if (currentSystem == "Sol")
+        {
+            thrusterControls.mainThrust = 100;
+            thrusterControls.portBowThrust = 50;
+            thrusterControls.starboardBowThrust = 17;
+            thrusterControls.portAftThrust = 50;
+            thrusterControls.starboardAftThrust = 17;
+        }
+        else if (currentSystem == "Sirius")
+        {
+            thrusterControls.mainThrust = 100;
+            thrusterControls.portBowThrust = 5;
+            thrusterControls.starboardBowThrust = 12;
+            thrusterControls.portAftThrust = 5;
+            thrusterControls.starboardAftThrust = 12;
+        }
+        else if (currentSystem == "Barnard's Star")
+        {
+            thrusterControls.mainThrust = 0;
+            thrusterControls.portBowThrust = 0;
+            thrusterControls.starboardBowThrust = 0;
+            thrusterControls.portAftThrust = 0;
+            thrusterControls.starboardAftThrust = 0;
+        }
     }
 }
