3b911f071252eba68b9b8a3618797804e9766832
Han Hao <han.hao@uwaterloo.ca>
2018-10-04 13:18:28 -0400

    Partially Integrated Defense with Sensors

--- Assets/Student Scripts/DefenceSubsystemController.cs
+++ Assets/Student Scripts/DefenceSubsystemController.cs
@@ -4,17 +4,17 @@ using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 using Sandbox;
 
-
 public class DefenceSubsystemController
 {
     public bool WillCrashIntoShip(SubsystemReferences subsystemReferences) {
-        Vector2 shipVelocityNorm = subsystemReferences.SensorsUpdate.forward;
-        Vector2 shipVelocity = subsystemReferences.SensorsUpdate.velocity;
-        Vector2 asteroidVelocityNorm = subsystemReferences.SensorsUpdate.vel.Normalize;
+        Vector2 shipVelocityNorm = subsystemReferences.forward;
+        Vector2 shipVelocity = subsystemReferences.velocity;
+        Vector2 shipPosition = subsystemReferences.currentShipPositionWithinGalaxyMapNode;
+        int asteroidVelocityNorm = (subsystemReferences.Sensors.EMSData[0].vel).magnitude;
         if (shipVelocityNorm == asteroidVelocityNorm || shipVelocityNorm == -asteroidVelocityNorm) {
             return false;
         }
-        float time = (shipPosition - subsystemReferences.SensorsUpdate.pos)/(subsystemReferences.sensorsUpdate.vel-shipVelocity);
+        float time = (shipPosition - subsystemReferences.Sensors.EMSData[0].pos)/(subsystemReferences.Sensors.EMSData[0].vel-shipVelocity).Normalize;
         if (time < 0) {
             return false;
         } else {
@@ -22,17 +22,18 @@ public class DefenceSubsystemController
         }
     }
     public float DistanceFromShip(SubsystemReferences subsystemReferences) {
-        float distance = (SubsystemRefences.SensorsUpdate.pos-shipPosition).magnitude;
+        float distance = ((subsystemReferences.Sensors.EMSData[0].pos) - (shipPosition)).magnitude;
         return distance;
     }
 
-    public Vector2 missileAim() {
-        turretControls.aimTo = DefenceSubsystemController,.pos;
+    public Vector2 missileAim(SubsystemReferences subsystemReferences, TurretControls turretControls) {
+        turretControls.aimTo = subsystemReferences.Sensors.EMSData[0].pos;
     }
-
     public void DefenceUpdate(SubsystemReferences subsystemReferences, TurretControls turretControls) {
-        turretControls.aimTo = new Vector3 (0,1,1);
+        //turretControls.aimTo = new Vector3 (0,1,1);
+        Vector2 aim = subsystemReferences.Sensors.EMSData[0].pos;
+        turretControls.aimTo = aim;
         turretControls.isTriggerPulled = true;
-        Ship.missileSpeed = 200;
+        Ship.missileSpeed = 20;
     }
 }
