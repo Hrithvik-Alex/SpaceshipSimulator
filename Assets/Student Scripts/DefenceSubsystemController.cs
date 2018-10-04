0c27858e62b9d0ef3d42b3297a0b54e71a26fefb
Han Hao <han.hao@uwaterloo.ca>
2018-10-04 11:56:44 -0400

    Halfway Integrated Defense with Sensors

--- Assets/Student Scripts/DefenceSubsystemController.cs
+++ Assets/Student Scripts/DefenceSubsystemController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
@@ -7,8 +7,32 @@ using Sandbox;
 
 public class DefenceSubsystemController
 {
-    public void DefenceUpdate(SubsystemReferences subsystemReferences, TurretControls turretControls)
-    {
+    public bool WillCrashIntoShip(SubsystemReferences subsystemReferences) {
+        Vector2 shipVelocityNorm = subsystemReferences.SensorsUpdate.forward;
+        Vector2 shipVelocity = subsystemReferences.SensorsUpdate.velocity;
+        Vector2 asteroidVelocityNorm = subsystemReferences.SensorsUpdate.vel.Normalize;
+        if (shipVelocityNorm == asteroidVelocityNorm || shipVelocityNorm == -asteroidVelocityNorm) {
+            return false;
+        }
+        float time = (shipPosition - subsystemReferences.SensorsUpdate.pos)/(subsystemReferences.sensorsUpdate.vel-shipVelocity);
+        if (time < 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    public float DistanceFromShip(SubsystemReferences subsystemReferences) {
+        float distance = (SubsystemRefences.SensorsUpdate.pos-shipPosition).magnitude;
+        return distance;
+    }
 
+    public Vector2 missileAim() {
+        turretControls.aimTo = DefenceSubsystemController,.pos;
+    }
+
+    public void DefenceUpdate(SubsystemReferences subsystemReferences, TurretControls turretControls) {
+        turretControls.aimTo = new Vector3 (0,1,1);
+        turretControls.isTriggerPulled = true;
+        Ship.missileSpeed = 200;
     }
 }
