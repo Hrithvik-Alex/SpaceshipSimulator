81e04c161b0ba9488d3b9b35b8baa6a5205b3abe
Han Hao <han.hao@uwaterloo.ca>
2018-10-04 14:05:27 -0400

    Added functionality for shooting closest asteroid

--- Assets/Student Scripts/DefenceSubsystemController.cs
+++ Assets/Student Scripts/DefenceSubsystemController.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using System;
 using System.Collections.Generic;
 using Vector2 = UnityEngine.Vector2;
@@ -6,31 +7,43 @@ using Sandbox;
 
 public class DefenceSubsystemController
 {
+    float shipVelocityNorm;
+    Vector2 shipVelocity;
+    Vector2 shipPosition;
+    float asteroidVelocityNorm;
+    float time;
     public bool WillCrashIntoShip(SubsystemReferences subsystemReferences) {
-        int shipVelocityNorm = subsystemReferences.forward;
-        Vector2 shipVelocity = subsystemReferences.velocity;
-        Vector2 shipPosition = subsystemReferences.currentShipPositionWithinGalaxyMapNode;
-        int asteroidVelocityNorm = (subsystemReferences.Sensors.EMSData[0].vel).magnitude;
-        float time = (shipPosition - subsystemReferences.Sensors.EMSData[0].pos)/(subsystemReferences.Sensors.EMSData[0].vel-shipVelocity).magnitude;
+        shipVelocityNorm = (subsystemReferences.forward).magnitude;
+        shipVelocity = subsystemReferences.velocity;
+        shipPosition = subsystemReferences.currentShipPositionWithinGalaxyMapNode;
+        asteroidVelocityNorm = (subsystemReferences.Sensors.EMSData[0].vel).magnitude;
+        time = ((shipPosition - subsystemReferences.Sensors.EMSData[0].pos)/(subsystemReferences.Sensors.EMSData[0].vel-shipVelocity)).magnitude;
         if (time < 0) {
             return false;
         } else {
             return true;
         }
     }
-    public float DistanceFromShip(SubsystemReferences subsystemReferences) {
-        float distance = ((subsystemReferences.Sensors.EMSData[0].pos) - (shipPosition)).magnitude;
-        return distance;
+    public Vector2 Asteroid(SubsystemReferences subsystemReferences) {
+        int asteroidCount = subsystemReferences.Sensors.EMSData.Count;
+        Vector2 closestAsteroid = subsystemReferences.Sensors.EMSData[0].pos;
+        for (int i = 1; i < asteroidCount; i++) {
+            float distance = ((subsystemReferences.Sensors.EMSData[i].pos) - (shipPosition)).magnitude;
+            if (distance < ((closestAsteroid) - (shipPosition)).magnitude) {
+                closestAsteroid = subsystemReferences.Sensors.EMSData[i].pos;
+            }
+        }
+        return closestAsteroid;
     }
 
-    public Vector2 missileAim(SubsystemReferences subsystemReferences, TurretControls turretControls) {
-        turretControls.aimTo = subsystemReferences.Sensors.EMSData[0].pos;
-    }
     public void DefenceUpdate(SubsystemReferences subsystemReferences, TurretControls turretControls) {
         //turretControls.aimTo = new Vector3 (0,1,1);
-        Vector2 aim = subsystemReferences.Sensors.EMSData[0].pos;
-        turretControls.aimTo = aim;
-        turretControls.isTriggerPulled = true;
-        Ship.missileSpeed = 20;
+        turretControls.aimTo = Asteroid(subsystemReferences);
+        if (subsystemReferences.Sensors.EMSData.Count == 0) {
+            turretControls.isTriggerPulled = false;
+            Debug.Log("false");
+        } else {
+            turretControls.isTriggerPulled = true;
+        }
     }
 }
